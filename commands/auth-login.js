93d5b8280ad2a10e536cba089ca70a40222434f6
Chris Erxleben <christopher.erxleben@bullhorn.com>
2019-03-18 14:08:03 -0500

    fix(login): To prevent node from exiting early

--- commands/auth-login.js
+++ commands/auth-login.js
@@ -1,17 +1,16 @@
 const chalk = require('chalk');
 const { checkLogin } = require('../lib/auth');
 
-const login = (credentials) => {
-    console.log(chalk.blue(`Authorizing...`));
-    // Push
-    return checkLogin(credentials)
-      .then((identity) => {
-        let rest = credentials.sessions.find(s => s.name === 'rest').value;
-        console.log(chalk.green(`Authorization Complete: ${rest.token}`));
-      })
-      .catch(error => {
-        console.log(chalk.red(error));
-      });
+const login = async (credentials) => {
+    try {
+      console.log(chalk.blue(`Authorizing...`));
+      // Push
+      await checkLogin(credentials);
+      const rest = credentials.sessions.find(s => s.name === 'rest').value;
+      console.log(chalk.green(`Authorization Complete: ${rest.token}`));
+    } catch (error) {
+      console.log(chalk.red(error));
+    }
 }
 
 // Export all methods
